54d9484a0d5cbd83c2b0d04f3459a59c559f77b0
erbaezla <goodfriend2210@hotmail.com>
2018-07-18 01:00:27 -0500

    subir archivos
    subir archivos

---
+++ index.php
@@ -0,0 +1,319 @@
+<!doctype html>
+<!--[if lt IE 7]><html lang="en" class="no-js ie6"><![endif]-->
+<!--[if IE 7]><html lang="en" class="no-js ie7"><![endif]-->
+<!--[if IE 8]><html lang="en" class="no-js ie8"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="en" class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="UTF-8">
+    <title>#AVANZA#IPUCVM</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]
+    <link rel="shortcut icon" href="favicon.ico">
+     <!--CSS de galería--> 
+    <link rel="stylesheet" href="asset/css/main.css" />
+   
+
+    <link rel="shortcut icon" href="./assets/img/vBH1woni_burned.ico">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/animate.css">
+    <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./assets/css/icomoon/style.css">
+    <link rel="stylesheet" href="./assets/css/jquery.easy-pie-chart.css">
+    <link rel="stylesheet" href="./assets/css/styles.css" title="mainStyle">
+    <link rel="stylesheet" href="./assets/css/styl.css">
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="./assets/rs-plugin/css/settings.css" media="screen" />
+
+    
+
+    <script src="js/modernizr.custom.32033.js"></script>
+
+    <!--[if IE]><script type="text/javascript" src="js/excanvas.compiled.js"></script><![endif]-->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+  </head>
+
+  <body>
+
+    <div class="pre-loader">
+        <div class="load-con">
+            <img src="./assets/img/logo-blue.png" class="animated fadeInDown" alt="">
+            <div class="spinner">
+                <div class="bounce1"></div>
+                <div class="bounce2"></div>
+                <div class="bounce3"></div>
+            </div>
+        </div>
+    </div>
+    <div class="go-down">
+        <a href="#whoWeAre">
+            <i class="fa fa-angle-down fa-3x"></i>
+        </a>
+    </div>
+    <!-- Wrap all page content here -->
+    <div id="wrap" class="home">
+
+        <header class="masthead">
+            <div class="slider-container" id="slider">
+                <div class="tp-banner-container">
+                    <div class="tp-banner">
+                        <ul>
+                            <li data-transition="slotfade-horizontal" data-slotamount="7" data-masterspeed="500">
+                                <!-- MAIN IMAGE -->
+                                <img src="./assets/img/samples/1.jpg" data-lazyload="./assets/img/samples/1.jpg" alt="slidebg1" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
+                                <!-- LAYERS -->
+
+
+
+                                <div class="tp-caption italic large_white_light sft hidden-xs" data-x="left" data-y="center" data-hoffset="0" data-voffset="-100" data-speed="700" data-start="1300" data-easing="Power4.easeOut">
+                                    
+                                </div>
+                                <div class="tp-caption italic large_white_light sfl hidden-xs" data-x="left" data-y="center" data-hoffset="0" data-voffset="-50" data-speed="700" data-start="1500" data-easing="Power4.easeOut">
+                                    
+                                </div>
+                                <div class="tp-caption italic large_white_light sfb hidden-xs" data-x="left" data-y="center" data-hoffset="147" data-voffset="-50" data-speed="700" data-start="1700" data-easing="Power4.easeOut">
+                                    
+                                </div>
+                                <div class="tp-caption italic large_white_light sfr hidden-xs" data-x="left" data-y="center" data-hoffset="335" data-voffset="-50" data-speed="700" data-start="1900" data-easing="Power4.easeOut">
+                                   
+                                </div>
+                                <div class="tp-caption lfb " data-x="left" data-y="center" data-hoffset="0" data-voffset="25" data-speed="1000" data-start="1900" data-easing="Power4.easeOut">
+                                    
+                                </div>
+
+                            </li>
+                            <li data-transition="slotfade-horizontal" data-slotamount="7" data-masterspeed="500">
+                                <!-- MAIN IMAGE -->
+                                <img src="./assets/img/samples/2.jpg" data-lazyload="./assets/img/samples/2.jpg" alt="slidebg1" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
+                                <!-- LAYERS -->
+
+                                <!-- LAYER NR. 1 -->
+                                <div class="tp-caption sft fadeout" data-x="center" data-y="center" data-hoffset="0" data-voffset="0" data-speed="500" data-start="700" data-easing="Power4.easeOut">
+                                    <img src="img/samples/macbook-bl.png" class="macbook-image" alt="">
+                                </div>
+
+                                <div class="tp-caption large_white_light italic sfb hidden-xs" data-x="left" data-y="center" data-hoffset="0" data-voffset="0" data-speed="1000" data-start="1300" data-easing="Power4.easeOut">
+                                    <a href="#portfolio" class="btn btn-naked"></a>
+                                </div>
+                                <div class="tp-caption italic large_white_light sft hidden-xs" data-x="right" data-y="center" data-hoffset="0" data-voffset="0" data-speed="1000" data-start="1300" data-easing="Power4.easeOut">
+                                    <a href="#team" class="btn btn-naked"></a>
+                                </div>
+                                </li>
+                                 <li data-transition="slotfade-horizontal" data-slotamount="7" data-masterspeed="500">
+                                <!-- MAIN IMAGE -->
+                                <img src="./assets/img/samples/4.jpg" data-lazyload="./assets/img/samples/4.jpg" alt="slidebg1" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
+                                <!-- LAYERS -->
+
+                                <!-- LAYER NR. 1 -->
+                                <div class="tp-caption sft fadeout" data-x="center" data-y="center" data-hoffset="0" data-voffset="0" data-speed="500" data-start="700" data-easing="Power4.easeOut">
+                                    <img src="img/samples/macbook-bl.png" class="macbook-image" alt="">
+                                </div>
+
+                                <div class="tp-caption large_white_light italic sfb hidden-xs" data-x="left" data-y="center" data-hoffset="0" data-voffset="0" data-speed="1000" data-start="1300" data-easing="Power4.easeOut">
+                                    <a href="#portfolio" class="btn btn-naked"></a>
+                                </div>
+                                <div class="tp-caption italic large_white_light sft hidden-xs" data-x="right" data-y="center" data-hoffset="0" data-voffset="0" data-speed="1000" data-start="1300" data-easing="Power4.easeOut">
+                                    <a href="#team" class="btn btn-naked"></a>
+                                </div>
+                            </li>
+                            <li data-transition="slotfade-horizontal" data-slotamount="7" data-masterspeed="500">
+                                <!-- MAIN IMAGE -->
+                                <img src="./assets/img/samples/3.jpg" data-lazyload="./assets/img/samples/3.jpg" alt="slidebg1" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
+                                <!-- LAYERS -->
+
+                                <!-- LAYER NR. 1 -->
+                                <div class="tp-caption lfl fadeout" data-x="left" data-y="center" data-hoffset="0" data-voffset="0" data-speed="500" data-start="700" data-easing="Power4.easeOut">
+                                    <img src="img/samples/iphone.png" alt="">
+                                </div>
+                                <div class="tp-caption large_white_bold sft" data-x="520" data-y="center" data-hoffset="0" data-voffset="-120" data-speed="500" data-start="1200" data-easing="Power4.easeOut">
+                                    <h1></h1>
+                                </div>
+                                <div class="tp-caption large_white_light sfb" data-x="520" data-y="center" data-hoffset="0" data-voffset="-40" data-speed="1000" data-start="1300" data-easing="Power4.easeOut">
+                                    
+                                </div>
+                                <div class="tp-caption large_white_light sfb" data-x="520" data-y="center" data-hoffset="0" data-voffset="10" data-speed="1000" data-start="1300" data-easing="Power4.easeOut">
+                                    
+                                </div>
+                                <div class="tp-caption large_white_light sfb" data-x="520" data-y="center" data-hoffset="0" data-voffset="60" data-speed="1000" data-start="1300" data-easing="Power4.easeOut">
+                                  
+                                </div>
+                                <div class="tp-caption sfb hidden-xs" data-x="520" data-y="center" data-hoffset="0" data-voffset="150" data-speed="1000" data-start="1700" data-easing="Power4.easeOut">
+                                    
+                                </div>
+                            </li>
+
+                        </ul>
+                        <div class="tp-bannertimer"></div>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- Fixed navbar -->
+            
+            <?php include('view/nav.php') ?> 
+            <!--/.navbar -->
+
+        </header>
+        <!-- Contenido -->
+
+        <?php include('view/content.php') ?> 
+
+        <section id="get-in-touch">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-push-2 clearfix">
+                        <div class="section-heading scrollpoint sp-effect3">
+                            <h1>.<span>C</span>ontacto</h1>
+                            
+                            <span class="divider"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="contact-details">
+                            <div class="detail">
+                                <h4>DIRECCIÓN</h4>
+                                    <p>CR 118 # 135 – 18,  Suba Villa María II Sector,  Bogotá, Colombia  </p>
+                                </div>
+                            <section>
+                                <div class="row">
+                                    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1ZYPhRqE2VwskO4nUQsrEQlRNSuE" width="400" height="300"></iframe>
+                                </div>
+                            </section>
+                               
+                                <div class="detail">
+                                    <h4>LLÁMANOS</h4>
+                                    <p>PASTOR: Edgar Quintero</p>
+                                    <p>(+57) 1 - 4701365</p>             
+
+                                </div>
+                                <div class="detail">
+                                    <h4>ESCRÍBENOS</h4>
+                                    <p>contacto@ipucvillamaria.com</p>
+                                </div>
+                                                                
+                                    <div class="detail">
+                                    <ul class="clearfix">
+                                    <li class="social-nav visible-lg">
+                                    <a href="https://www.facebook.com/paginaipucvillamaria/?fref=ts" target="_blank"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://twitter.com/IPUCVillaMaria" target="_blank"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://www.youtube.com/channel/UCHicWsR5Xsp-NI8DSSMiTjg" target="_blank"><i class="fa fa-youtube"></i></a>
+                                    <a href="https://www.instagram.com/ipucvm/" target="_blank"><i class="fa fa-camera"></i></a>                        
+                                   </li>
+                               </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                    <br>
+                        <br>
+                                <br>
+                            <form>
+                                <div class="form-group has-feedback left">
+                                    <input type="text" class="form-control" placeholder="NOMBRE">
+                                    <i class="fa fa-user form-control-feedback"></i>
+                                </div>
+                                <div class="form-group has-feedback left">
+                                    <input type="email" class="form-control" placeholder="CORREO">
+                                    <i class="fa fa-envelope-o form-control-feedback"></i>
+                                </div>
+                                <div class="form-group has-feedback left">
+                                    <textarea class="form-control" rows="7" placeholder="MENSAJE"></textarea>
+                                    <i class="fa fa-pencil-square-o form-control-feedback"></i>
+                                </div>
+                                <button class="btn btn-primary btn-lg pull-right" type="submit">ENVIAR</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--mapa-->
+            
+        
+     <!--footer-->
+    <?php include('view/footer.php') ?>
+
+       
+     
+
+     <!--fagpage facebook-->
+        <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9";
+              fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+      </script>  
+
+      <div id="fb-root"></div>
+      <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.12';
+          fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Scripts de galería-->
+    <script src="asset/js/jquery.min.js"></script>
+     <script src="asset/js/jquery.poptrox.min.js"></script>
+     <script src="asset/js/skel.min.js"></script>
+     <script src="asset/js/util.js"></script>
+    <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+     <script src="asset/js/main.js"></script>
+
+
+
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="./assets/js/bootstrap.min.js"></script>
+      <script src="./assets/js/stellar.js"></script>
+      <script src="./assets/js/isotope.pkgd.min.js"></script>
+      <script src="./assets/js/jquery.easypiechart.min.js"></script>
+
+      <!-- jQuery REVOLUTION Slider  -->
+      <script type="text/javascript" src="./assets/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+      <script type="text/javascript" src="./assets/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+      <script src="./assets/js/waypoints.min.js"></script>
+      <script src="./assets/js/script.js"></script>
+
+
+      <script>
+        $(document).ready(function() {
+            appMaster.preLoader();
+            appMaster.smoothScroll();
+            appMaster.animateScript();
+            appMaster.navSpy();
+            appMaster.revSlider();
+            appMaster.stellar();
+            appMaster.skillsChart();
+            appMaster.isoTop();
+            appMaster.canvasHack();
+        });
+    </script>
+
+    <!-- jQuery REVOLUTION Slider  -->
+
+    
+
+
+</body>
+</html>
+
